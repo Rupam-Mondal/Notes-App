402cfd9729746c3d20cdbbc29ae2b919561cc3f3
Rupam <rupammondal564@gmail.com>
2024-07-21 08:26:53 +0530

    Notes Taking application

---
+++ script.js
@@ -0,0 +1,6 @@
+//Note add box section
+const noteadd = document.querySelector('.Notes-addition')
+noteadd.addEventListener('click' , () => {
+    const noddaddbox = document.querySelector('.Note-input')
+    noddaddbox.style.display = "block"
+})
